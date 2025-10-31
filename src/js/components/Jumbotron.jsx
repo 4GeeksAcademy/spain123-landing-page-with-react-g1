381eaf6f609c62dea8152abe9c25f11a707be515
andreasabater <155462711+andreasabater@users.noreply.github.com>
2025-10-31 19:28:22 +0000

    jumbotron

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+export const Jumbotron = () => {
+  return (
+    <div className="bg-light p-5 rounded-3 shadow-sm mt-4">
+      <div className="container py-5">
+        <h1 className="display-4 fw-bold">¡Bienvenido a mi página!</h1>
+        <p className="lead">
+          ejemplo y más ejemplo 
+        </p>
+        <hr className="my-4" />
+        <p>
+         hola hola hola hola 
+        </p>
+        <a className="btn btn-primary btn-lg" href="#!" role="button">
+          weeeeeeeeeb
+        </a>
+      </div>
+    </div>
+  );
+};
+
+
+
